3934bde025b4810d0c2d12753a20a46acb899db3
Andrew Lake <jamboarder@gmail.com>
2011-08-22 17:06:10 -0700

    add EventTranslator, BehaviorTranslator and ResourceManager class to repo

---
+++ src/BehaviorTranslator.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+
+public class BehaviorTranslator {
+	
+	public boolean behave(List<Event> events) {
+		//TODO: Finish implementation
+		return true;
+	}
+}
---
+++ src/ResourceManager.java
@@ -0,0 +1,17 @@
+
+public class ResourceManager {
+	double costScale = 1.0;
+	
+	public int stepCost() {
+		//TODO: Finish implementation
+		return 0;
+	}
+	
+	public void setCostScale(double scale) {
+		//TODO: Finish implementation
+		if (scale >= 0 || scale <= 1.0)  {
+			costScale = scale;
+		}
+		return;
+	}
+}
